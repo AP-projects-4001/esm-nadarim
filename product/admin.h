4b8dafb2881d9ae7667240a97057bdbe652d40a7
mohamad taha narimani <94551674+mtahana@users.noreply.github.com>
2022-06-25 23:14:02 +0430

    debuge and add class admin

---
+++ product/admin.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "product.h"
+#include "fileProduct.h"
+
+namespace bak {
+
+	class admin {
+
+	public:
+		std::vector<product>allTransactions();
+
+	private:
+		product transactionsProduct;
+
+	};
+
+}
