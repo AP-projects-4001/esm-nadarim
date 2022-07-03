e5f41152d46dd3e3ba7a55b5a23daeb93b421b49
AmirrezaGholizade <amirrezabyhimself@gmail.com>
2022-07-03 12:49:30 +0430

    fixing some bugs

--- Login_And_Admin/main.cpp
+++ Login_And_Admin/main.cpp
@@ -11,6 +11,16 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
 
+    //open qss file
+    /*QFile file("C:/Users/amirr/Desktop/Codes/Hookmark.qss");
+    file.open(QFile::ReadOnly);
+
+    QString styleSheet=QLatin1String(file.readAll());
+
+    //setup stylesheet
+    a.setStyleSheet(styleSheet);*/
+
+
     MainWindow w;
     w.show();
     return a.exec();
